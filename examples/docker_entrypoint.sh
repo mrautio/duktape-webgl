1dc02639a1a236a6873cc3f099c23a199de426f5
Mika Rautio <mika.rautio@gmail.com>
2020-04-15 14:38:42 +0300

    Refine test validation script

--- examples/docker_entrypoint.sh
+++ examples/docker_entrypoint.sh
@@ -19,8 +19,20 @@ screenshot_diff ()
 	ACTUAL="$1"
 	EXPECTED="$2"
 
+	if [ ! -f $EXPECTED ]
+	then
+		echo "Screenshot missing! '${EXPECTED}'"
+		exit 3
+	fi
+
 	# convert raw format and flip the image
-	convert -size 640x480 -depth 8 +flip rgba:tmpscreenshot_000000.rgb $1
+	convert -size 640x480 -depth 8 +flip rgba:tmpscreenshot_000000.rgb $1 || assert_ret $? 0
+
+	if [ ! -f $ACTUAL ]
+	then
+		echo "Screenshot missing! '${ACTUAL}'"
+		exit 3
+	fi
 
 	# compare screenshots for differences, allow small color differences with fuzzing
 	compare -verbose -metric AE -fuzz 1% $ACTUAL $EXPECTED null: 2>&1 || assert_ret $? 0
@@ -37,7 +49,7 @@ sleep 5s
 00_bootstrap/bootstrap.exe 00_bootstrap/exit_success.js
 00_bootstrap/bootstrap.exe 00_bootstrap/exit_failure.js || assert_ret $? 1 
 00_bootstrap/bootstrap.exe 00_bootstrap/exit_failure_invalid_gl_call.js || assert_ret $? 1
-00_bootstrap/bootstrap.exe 01_grey_screen/grey_screen.js && screenshot_diff screenshot_01_actual.png 01_grey_screen/screenshot_grey_screen.png
-00_bootstrap/bootstrap.exe 02_hello_triangle/hello_triangle.js && screenshot_diff screenshot_02_actual.png 02_hello_triangle/screenshot_hello_triangle.png
-00_bootstrap/bootstrap.exe 03_draw_image/draw_image.js && screenshot_diff screenshot_03_actual.png 03_draw_image/screenshot_draw_image.png
-00_bootstrap/bootstrap.exe 04_render_to_texture/render_to_texture.js && screenshot_diff screenshot_04_actual.png 04_render_to_texture/screenshot_render_to_texture.png
+00_bootstrap/bootstrap.exe 01_grey_screen/grey_screen.js || assert_ret $? 1 && screenshot_diff screenshot_01_actual.png 01_grey_screen/screenshot_grey_screen.png
+00_bootstrap/bootstrap.exe 02_hello_triangle/hello_triangle.js || assert_ret $? 1 && screenshot_diff screenshot_02_actual.png 02_hello_triangle/screenshot_hello_triangle.png
+00_bootstrap/bootstrap.exe 03_draw_image/draw_image.js || assert_ret $? 1 && screenshot_diff screenshot_03_actual.png 03_draw_image/screenshot_draw_image.png
+00_bootstrap/bootstrap.exe 04_render_to_texture/render_to_texture.js || assert_ret $? 1 && screenshot_diff screenshot_04_actual.png 04_render_to_texture/screenshot_render_to_texture.png
